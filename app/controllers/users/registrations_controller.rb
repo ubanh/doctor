e03008470892af051f24864efc7527f4bb23a5c9
Uban <uban@magaransoft.com>
2017-08-14 11:24:23 -0400

    path after invalid sign up

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
-  layout "sign", only: :new
+  layout "sign", only: [:new, :create]
 
   # GET /resource/sign_up
   # def new
